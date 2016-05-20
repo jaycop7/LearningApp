cfffad0e136945cad0c4f0f64770dad1cd4232e6
pwachholz <patrick.wachholz@edu.campus02.at>
2016-05-20 16:32:47 +0200

    Test

--- app/src/main/java/at/campus02/gang_of_four/learningapp/FrageErstellenActivity.java
+++ app/src/main/java/at/campus02/gang_of_four/learningapp/FrageErstellenActivity.java
@@ -8,6 +8,7 @@ public class FrageErstellenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // test
         setContentView(R.layout.activity_frage_erstellen);
     }
 }
