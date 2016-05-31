1fe8d871e8ace11c0864759e4b05041f0fe6714a
mightyphlip <p.trummer@edu.campus02.at>
2016-05-31 21:47:51 +0200

    frage erstellen funktion 1

--- app/src/main/java/at/campus02/gang_of_four/learningapp/FrageErstellenActivity.java
+++ app/src/main/java/at/campus02/gang_of_four/learningapp/FrageErstellenActivity.java
@@ -1,16 +1,97 @@
 package at.campus02.gang_of_four.learningapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import at.campus02.gang_of_four.learningapp.model.Frage;
+import at.campus02.gang_of_four.learningapp.model.Schwierigkeit;
+import at.campus02.gang_of_four.learningapp.rest.RestDataService;
+import at.campus02.gang_of_four.learningapp.rest.restListener.SuccessListener;
+import at.campus02.gang_of_four.learningapp.utils.Preferences;
 
 public class FrageErstellenActivity extends AppCompatActivity {
+
+
+    EditText frage = null;
+    EditText antwort = null;
+    Spinner schwierigkeit = null;
+    EditText kategorie = null;
+    EditText bild = null;
+
+    Frage newFrage = null;
+
+    RestDataService service = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // test
-        // rest Jakob
-        // test test phips 5
         setContentView(R.layout.activity_frage_erstellen);
+
+        frage = (EditText)findViewById(R.id.editText_Frage);
+        antwort = (EditText)findViewById(R.id.editText_Antwort);
+     //   schwierigkeit = (Spinner)findViewById(R.id.schwierigkeit);
+
+         bild = (EditText)findViewById(R.id.editText_bild);
+
+
+        newFrage = new Frage();
+
+        service = new RestDataService(this);
+
     }
+
+    public void frageSpeichern(View view) {
+        newFrage.setFragetext(frage.getText().toString());
+        newFrage.setAntwort(antwort.getText().toString());
+        newFrage.setLaengenUndBreitengrad("default");
+        newFrage.setBild(bild.getText().toString());
+        newFrage.setKategorie("tbd");
+        newFrage.setSchwierigkeitsgrad(1);
+        newFrage.setFrageID("1");
+
+
+        showSucessMessage();
+
+        service.createFrage(newFrage, new SuccessListener() {
+            @Override
+            public void success() {
+
+                showSucessMessage();
+
+            }
+
+            @Override
+            public void error() {
+
+                showFailMessage();
+
+            }
+
+
+        });
+
+        //redirect to main activity
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void showSucessMessage() {
+        String info = getString(R.string.frage_erstellt);
+        Toast toast = Toast.makeText(this, info, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+
+    private void showFailMessage() {
+        String fail = getString(R.string.frage_nicht_erstellt);
+        Toast toast = Toast.makeText(this, fail, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+
 }
