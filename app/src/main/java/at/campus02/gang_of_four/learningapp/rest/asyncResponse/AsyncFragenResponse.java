a4f6e9f0afcf08d1fc276c7748fecca5d5e8ba56
mightyphlip <p.trummer@edu.campus02.at>
2016-05-31 21:46:12 +0200

    jakob fehler ausgebessert

--- app/src/main/java/at/campus02/gang_of_four/learningapp/rest/asyncResponse/AsyncFragenResponse.java
+++
@@ -1,9 +0,0 @@
-package at.campus02.gang_of_four.learningapp.rest.asyncResponse;
-
-import java.util.List;
-
-import at.campus02.gang_of_four.learningapp.model.Frage;
-
-public interface AsyncFragenResponse extends AsyncResponse {
-    void processResponse(List<Frage> fragen);
-}
