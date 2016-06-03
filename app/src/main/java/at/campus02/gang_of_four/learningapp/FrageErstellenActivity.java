1aa78d2b31ca3c0ec7d622047e9e4b991b6d295e
Jakob <jakob.aumueller@gmail.com>
2016-06-03 11:29:58 +0200

    Eigene Frage an Maintenance weiterleiten

--- app/src/main/java/at/campus02/gang_of_four/learningapp/FrageErstellenActivity.java
+++ app/src/main/java/at/campus02/gang_of_four/learningapp/FrageErstellenActivity.java
@@ -1,10 +1,12 @@
 package at.campus02.gang_of_four.learningapp;
 
+import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -12,22 +14,30 @@ import android.widget.TextView;
 import java.util.List;
 
 import at.campus02.gang_of_four.learningapp.model.Frage;
+import at.campus02.gang_of_four.learningapp.model.FrageMaintenanceModus;
 import at.campus02.gang_of_four.learningapp.model.Schwierigkeit;
 import at.campus02.gang_of_four.learningapp.rest.RestDataService;
+import at.campus02.gang_of_four.learningapp.rest.restListener.FrageListener;
 import at.campus02.gang_of_four.learningapp.rest.restListener.SuccessListener;
 import at.campus02.gang_of_four.learningapp.utils.Utils;
 
 public class FrageErstellenActivity extends AppCompatActivity {
+    public static final String EXTRA_MAINTENANCE_MODE = "at.campus02.gang_of_four.learningapp.FrageMaintenanceModus";
+    public static final String EXTRA_FRAGE_ID = "at.campus02.gang_of_four.learningapp.FrageId";
 
-    EditText frage = null;
-    EditText antwort = null;
-    Spinner schwierigkeit = null;
-    EditText kategorie = null;
-    EditText bild = null;
-    TextView position = null;
+    EditText frageView = null;
+    EditText antwortView = null;
+    Spinner schwierigkeitView = null;
+    EditText kategorieView = null;
+    EditText bildView = null;
+    TextView positionView = null;
+    Button speichernButton = null;
 
     String coordinate = null;
     Frage newFrage = null;
+    Frage editFrage = null;
+    FrageMaintenanceModus maintenanceModus = null;
+    String editFrageId = null;
 
     RestDataService service = null;
 
@@ -39,6 +49,19 @@ public class FrageErstellenActivity extends AppCompatActivity {
         populateSchwierigkeitSpinner();
         linkLayoutViews();
 
+        Intent intent = getIntent();
+        retrieveIntentExtra(intent);
+        if (maintenanceModus == FrageMaintenanceModus.CREATE) {
+            fillCurrentLocation();
+            speichernButton.setText(getText(R.string.erstellen_speichern));
+            newFrage = new Frage();
+        } else {
+            loadEditFrage();
+            speichernButton.setText(getText(R.string.frage_edit_speichern));
+        }
+    }
+
+    private void fillCurrentLocation() {
         Location location = Utils.getCurrentLocation(this);
         if (location != null) {
             String latidue = String.valueOf(location.getLatitude());
@@ -50,26 +73,70 @@ public class FrageErstellenActivity extends AppCompatActivity {
             coordinate = getString(R.string.erstellen_aktuelle_position);
 
         }
-        position.setText(coordinate);
-        newFrage = new Frage();
+        positionView.setText(coordinate);
     }
 
     private void linkLayoutViews() {
-        frage = (EditText) findViewById(R.id.editText_Frage);
-        antwort = (EditText) findViewById(R.id.editText_Antwort);
-        schwierigkeit = (Spinner) findViewById(R.id.editText_Schwierigkeit);
-        kategorie = (EditText) findViewById(R.id.editText_Kategorie);
-        bild = (EditText) findViewById(R.id.editText_bild);
-        position = (TextView) findViewById(R.id.textView_aktuelle_position);
+        frageView = (EditText) findViewById(R.id.editText_Frage);
+        antwortView = (EditText) findViewById(R.id.editText_Antwort);
+        schwierigkeitView = (Spinner) findViewById(R.id.editText_Schwierigkeit);
+        kategorieView = (EditText) findViewById(R.id.editText_Kategorie);
+        bildView = (EditText) findViewById(R.id.editText_bild);
+        positionView = (TextView) findViewById(R.id.textView_aktuelle_position);
+        speichernButton = (Button) findViewById(R.id.maintenanceSpeichernButton);
+    }
+
+    private void retrieveIntentExtra(Intent intent) {
+        maintenanceModus = (FrageMaintenanceModus) intent.getSerializableExtra(EXTRA_MAINTENANCE_MODE);
+        editFrageId = intent.getStringExtra(EXTRA_FRAGE_ID);
+    }
+
+    private void loadEditFrage() {
+        if (editFrage != null && !editFrageId.isEmpty())
+            service.getFrage(editFrageId, new FrageListener() {
+                @Override
+                public void success(Frage frage) {
+                    editFrageGeladen(frage);
+                }
+
+                @Override
+                public void error() {
+                    editFrageError();
+                }
+            });
+        else
+            editFrageError();
+    }
+
+    private void editFrageGeladen(Frage frage) {
+        editFrage = frage;
+        frageView.setText(frage.getFragetext());
+        antwortView.setText(frage.getAntwort());
+        kategorieView.setText(frage.getKategorie());
+        positionView.setText(frage.getLaengenUndBreitengrad());
+        int schwierigkeitId = frage.getSchwierigkeitsgrad();
+        int index = 0;
+        List<Schwierigkeit> schwierigkeiten = Utils.getSchwierigkeiten();
+        for (Schwierigkeit s : schwierigkeiten) {
+            if (s.getId() == schwierigkeitId)
+                break;
+            index++;
+        }
+        schwierigkeitView.setSelection(index);
+        bildView.setText(frage.getBild());
+    }
+
+    private void editFrageError() {
+        Utils.showToast(getString(R.string.frage_maintenance_error_loading), this);
     }
 
     public void frageSpeichern(View view) {
-        newFrage.setFragetext(frage.getText().toString());
-        newFrage.setAntwort(antwort.getText().toString());
+        newFrage.setFragetext(frageView.getText().toString());
+        newFrage.setAntwort(antwortView.getText().toString());
         newFrage.setLaengenUndBreitengrad("default");
-        newFrage.setBild(bild.getText().toString());
-        newFrage.setKategorie(kategorie.getText().toString());
-        newFrage.setSchwierigkeitsgrad(((Schwierigkeit) schwierigkeit.getSelectedItem()).getId());
+        newFrage.setBild(bildView.getText().toString());
+        newFrage.setKategorie(kategorieView.getText().toString());
+        newFrage.setSchwierigkeitsgrad(((Schwierigkeit) schwierigkeitView.getSelectedItem()).getId());
         newFrage.setFrageID("1");
 
         service.createFrage(newFrage, new SuccessListener() {
---
+++ app/src/main/java/at/campus02/gang_of_four/learningapp/model/FrageMaintenanceModus.java
@@ -0,0 +1,5 @@
+package at.campus02.gang_of_four.learningapp.model;
+
+public enum FrageMaintenanceModus {
+    CREATE, EDIT
+}
